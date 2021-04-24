00a2b06a4b5dd51b0cf7725041ae4c6c3b5f3a76
rizvi23061998 <rizvi23061998@gmail.com>
2021-04-24 11:27:29 +0600

    test 1.3 seed set for split

--- src_MuSeAM/model.py
+++ src_MuSeAM/model.py
@@ -110,7 +110,7 @@ class nn_model:
         
 
         #Reproducibility    
-        seed = 1234#random.randint(1,1000)
+        self.seed = 1234#random.randint(1,1000)
 
         np.random.seed(seed)
         tf.random.set_seed(seed)
@@ -241,7 +241,8 @@ class nn_model:
         rc_fasta = dict["reverse"]
         readout = dict["readout"]
 
-        seed = random.randint(1,1000)
+        seed = self.seed#random.randint(1,1000)
+        
 
         x1_train, x1_test, y1_train, y1_test = train_test_split(fw_fasta, readout, test_size=0.1, random_state=seed)
         # split for reverse complemenet sequences
